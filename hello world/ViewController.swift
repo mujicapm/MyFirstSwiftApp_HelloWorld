00c524b5bd38c15d48a96da3cac50533d5d1fa42
Mauro III <mauro.mujica.3@gmail.com>
2022-01-10 16:11:19 -0600

    Initial Commit

---
+++ hello world/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hello world
+//
+//  Created by Mauro Mujica-Parodi on 1/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
